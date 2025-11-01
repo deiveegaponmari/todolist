e00a2d6451ca4758a699cbe654cfda099f428333
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-11-01 15:33:20 +0530

    changes added

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,44 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import TaskList from './TaskList'
+import React, { useEffect, useState } from "react";
+import TaskList from "./TaskList";
 
 function CreateTask() {
-    const[data,setData]=useState([]);
-    const[category,setCategory]=useState("");
-    const[task,setTask]=useState("");
-    const addTask=()=>{
-        if(task.trim()==="") return;
-        const newTask={
-            id:Date.now(),
-            text:task,
-            category:category,
-            completed:false
-        }
-        setData(prevtask=>[...prevtask,newTask]);
-        setTask("");//clear user input
-        setCategory("");//clear category
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState("");
+  const [task, setTask] = useState("");
+  const addTask = () => {
+    if (task.trim() === "") return;
+    const newTask = {
+      id: Date.now(),
+      text: task,
+      category: category,
+      completed: false,
+    };
+    const updatedTask = [...data, newTask];
+    setData(updatedTask);
+    localStorage.setItem("taskList", JSON.stringify(updatedTask));
+    setTask(""); //clear user input
+    setCategory(""); //clear category
+  };
+  useEffect(() => {
+    const getTasks = JSON.parse(localStorage.getItem("taskList"));
+    if (getTasks) {
+      setData(getTasks);
     }
-  
+  }, []);
+  function editItem(index) {
+    console.log("index is",index);
+    
+  }
+  function deleteItem(index) {
+   const updated= data.filter((list)=>list.id!==index)
+    setData(updated);
+    localStorage.setItem("taskList",JSON.stringify(updated))
+  }
   return (
-    <div className='bg-white w-xl p-5 h-64'>
-        <h1 className='text-3xl font-bold '>To Do List</h1>
-        <div className='bg-white-200 flex flex-col justify-center'>
-           {/*  select category */}
-        <select value={category}  onChange={(e)=>setCategory(e.target.value)}>
-             <option value=''>Select Category</option>
-            <option value='health'>Health</option>
-              <option value='skill'>Skill</option>
-                <option value='selfcare'>SelfCare</option>
-                  <option value='others'>Others</option>
+    <div className="bg-white w-xl p-5 h-screen mt-30 mb-10">
+      <h1 className="text-3xl font-bold ">To Do List</h1>
+      <div className="bg-white-200 flex flex-col justify-center">
+        {/*  select category */}
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Select Category</option>
+          <option value="health">Health</option>
+          <option value="skill">Skill</option>
+          <option value="selfcare">SelfCare</option>
+          <option value="others">Others</option>
         </select>
-      {/*   user input ---add task here */}
-        <input type='text' value={task}  placeholder='Enter Task Here' onChange={(e)=>setTask(e.target.value)}/>
-        <button className='bg-gradient-to-r from-blue-500 to-indigo-200 text-white'
-        onClick={addTask}> Add Task</button>
+        {/*   user input ---add task here */}
+        <input
+          type="text"
+          value={task}
+          placeholder="Enter Task Here"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button
+          className="bg-gradient-to-r from-blue-500 to-indigo-200 text-white"
+          onClick={addTask}
+        >
+          Add Task
+        </button>
         {/* Task list display here */}
-        <TaskList data={data}/>
-        </div>
+        <TaskList data={data} editItem={editItem} deleteItem={deleteItem} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateTask
+export default CreateTask;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,42 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React from "react";
 
 function TaskList(props) {
-     const[taskData,setTaskData]=useState([]);
-       useEffect(()=>{
-        if(props.data.length>0){
-            setTaskData(props.data)
-        localStorage.setItem("taskList",JSON.stringify(props.data))
-        }
-    },[props.data])
-   useEffect(()=>{
-    const getTasks=JSON.parse(localStorage.getItem("taskList"));
-    if(getTasks){
-        setTaskData(getTasks)
-    }
-   },[]) 
   return (
-    <div className='flex flex-col'>
-        <h1>Your Task Today</h1>
-        <ul>
-            {/* dynamically added list here */}
-            {
-               taskData.length===0 ? "No Task Added" :
-            
-                taskData.map((list,index)=>{
-                    return <div key={list.id} className='flex'>
-                        <input type='checkbox'/>
-                        <div className='flex flex-col'>
-                        <h1>{list.text}</h1>
-                        <h5>{list.category}</h5>
-                        </div>
-                        <button>Edit</button>
-                         <button>Delete</button>
-                    </div>
-                })
-            }
-        </ul>
+    <div className="flex flex-col justify-center p-5">
+      <h1>Your Task Today</h1>
+      <ul>
+        {/* dynamically added list here */}
+        {props.data.length === 0
+          ? "No Task Added"
+          : props.data.map((list, index) => {
+              return (
+                <div key={list.id} className="flex gap-2 ">
+                  <input type="checkbox" />
+                  <div className="flex flex-col">
+                    <h1>{list.text}</h1>
+                    <h5>{list.category}</h5>
+                  </div>
+                  <button
+                    className="bg-yellow-300 w-14 h-8 rounded"
+                    onClick={() => props.editItem(list.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bg-red-300 w-14 h-8 rounded"
+                    onClick={() => props.deleteItem(list.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
